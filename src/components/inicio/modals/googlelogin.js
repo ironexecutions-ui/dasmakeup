4384e8ac8d80f00b53dab292d06cfdf2903707e9
Andy de Oliveira <elderromero565@gmail.com>
2026-01-21 22:36:06 -0300

    edsessdddsddd

--- src/components/inicio/modals/googlelogin.js
+++ src/components/inicio/modals/googlelogin.js
@@ -1,35 +1,30 @@
-import { useEffect } from "react";
 import { API_URL } from "../../../config";
 
-export default function LoginGoogle({ onSuccess }) {
-    useEffect(() => {
-        if (!window.google) return;
+export function iniciarGoogleLogin(callback) {
+    /* global googleffv */
 
-        google.accounts.id.initialize({
-            client_id: "992388111982-a0hootauhov3044flkk0pr4ndtsqiug7.apps.googleusercontent.com",
-            callback: async (response) => {
-                const credential = response.credential;
+    google.accounts.id.initialize({
+        client_id: "992388111982-a0hootauhov3044flkk0pr4ndtsqiug7.apps.googleusercontent.com",
+        callback: async (response) => {
+            const credential = response.credential;
 
-                const resp = await fetch(`${API_URL}/das/auth/google`, {
-                    method: "POST",
-                    headers: { "Content-Type": "application/json" },
-                    body: JSON.stringify({ credential })
-                });
+            const resp = await fetch(`${API_URL}/das/auth/google`, {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ credential })
+            });
 
-                const json = await resp.json();
+            const json = await resp.json();
 
-                if (json.token) {
-                    localStorage.setItem("token", json.token);
-                    onSuccess?.();
-                }
+            if (json.token) {
+                localStorage.setItem("token", json.token);
+                callback(true);
+            } else {
+                callback(false);
             }
-        });
+        }
+    });
 
-        google.accounts.id.renderButton(
-            document.getElementById("googleBtn"),
-            { theme: "outline", size: "large" }
-        );
-    }, []);
 
-    return <div id="googleBtn"></div>;
+    google.accounts.id.prompt();
 }
